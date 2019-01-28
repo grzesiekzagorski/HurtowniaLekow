a42d0925932122095bdf262a3bad7974dcfde802
Grzesiek <grzesiek_zagorski1@o2.pl>
2019-01-28 17:32:52 +0100

    dodanie funkcjonalnosci wyswietlania zalogowanego uzytkownika

--- src/main/java/pl/zagorski/controllers/MedicineController.java
+++ src/main/java/pl/zagorski/controllers/MedicineController.java
@@ -1,15 +1,15 @@
 package pl.zagorski.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import pl.zagorski.services.CharacterServiceImpl;
-import pl.zagorski.services.MedicineServiceImpl;
-import pl.zagorski.services.PrescriptionServiceImpl;
-import pl.zagorski.services.ProducerServiceImpl;
+import pl.zagorski.domain.Employee;
+import pl.zagorski.services.*;
 
 @Controller
 public class MedicineController {
@@ -22,13 +22,27 @@ public class MedicineController {
     private CharacterServiceImpl characterService;
     @Autowired
     private ProducerServiceImpl producerService;
+    @Autowired
+    private EmployeeServiceImpl employeeService;
+
 
     @RequestMapping(value ="/medicine/allMedicines",method = RequestMethod.GET)
     public String findAllMedicines(Model model) {
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+
+        } else {
+            username = principal.toString();
+        }
+
         model.addAttribute("medicines", medicineService.showAllMedicines());
         model.addAttribute("characters", characterService.findAll());
         model.addAttribute("producers", producerService.findAll());
         model.addAttribute("prescriptions", prescriptionService.findAll());
+        model.addAttribute("user",employeeService.getEmployeeByLogin(username).get());
         return "allMedicines";
     }
 
@@ -56,6 +70,15 @@ public class MedicineController {
         model.addAttribute("characters", characterService.findAll());
         model.addAttribute("producers", producerService.findAll());
         model.addAttribute("prescriptions", prescriptionService.findAll());
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+
+        } else {
+            username = principal.toString();
+        }
+        model.addAttribute("user",employeeService.getEmployeeByLogin(username).get());
         return "allMedicines";
     }
     @RequestMapping(value ="/medicine/allMedicines",params = "idMedicineDelete", method = RequestMethod.POST)
